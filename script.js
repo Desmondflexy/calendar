f88a91ebe5d9d7693bee37065a86d342db0ff16f
Desmondflexy <ikilodesmond@gmail.com>
2023-05-05 17:23:37 +0100

    update

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const selectMonth = document.querySelector('select');
 const yearInput = document.querySelector('input');
+const weekdays = document.querySelector('.weekdays');
 const timeNow = document.querySelector('footer');
-
+const calendarHeader = document.querySelector('.month-year');
+const days = document.querySelector('.days');
 const months = [
     'January',
     'February',
@@ -16,11 +18,17 @@ const months = [
     'November',
     'December'
 ];
+
 months.forEach(month => {
     const option = document.createElement('option');
     option.innerHTML = option.value = month;
     selectMonth.append(option);
-})
+});
+['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'].forEach(wk => {
+    const weekday = document.createElement('li');
+    weekday.innerHTML = wk;
+    weekdays.append(weekday);
+});
 
 let now = new Date();
 let month = months[now.getMonth()];
@@ -29,9 +37,10 @@ createCalendar();
 
 selectMonth.addEventListener('change', () => yearInput.focus());
 document.querySelector('form').addEventListener('submit', handleSubmit);
+yearInput.addEventListener('input', () => yearInput.value = Number(yearInput.value).toString().padStart(4, 0));
 
 document.querySelector('.prev').addEventListener('click', () => {
-    if (year.toString() === yearInput.getAttribute('min') && month === months[0]){
+    if (year.toString() === yearInput.getAttribute('min') && month === months[0]) {
         return;
     }
     let m = months.indexOf(month) - 1;
@@ -42,10 +51,10 @@ document.querySelector('.prev').addEventListener('click', () => {
         month = months[m];
     }
     createCalendar();
-})
+});
 
 document.querySelector('.next').addEventListener('click', () => {
-    if (year.toString() === yearInput.getAttribute('max') && month === months[11]){
+    if (year.toString() === yearInput.getAttribute('max') && month === months[11]) {
         return;
     }
     let m = months.indexOf(month) + 1;
@@ -56,71 +65,33 @@ document.querySelector('.next').addEventListener('click', () => {
         month = months[m];
     }
     createCalendar();
-})
+});
 
 timeNow.addEventListener('click', () => {
     month = months[now.getMonth()];
     year = now.getFullYear();
     createCalendar();
-})
+});
 
-// Changes the time display on the footer every second
+// Updates the value of 'now' every second
 setInterval(() => {
     now = new Date();
     timeNow.innerHTML = `${now.toDateString()}, ${now.toLocaleTimeString()}`;
-}, 1000);
+}, 1);
 
 function createCalendar() {
-    // Validates inputs entered from the browser console. Just for debugging sakes.
-    if (!(months.includes(month)) || year < yearInput.getAttribute('min') || year > yearInput.getAttribute('max')) {
-        throw ('Error in createCalendar: Invalid input.')
-    }
+    const [countdays, daysOfMonth] = countDateTime();
+    const year0 = year.toString().padStart(4, 0);
 
-    // Update the values on the form
     selectMonth.value = month;
-    yearInput.value = year;
-    document.querySelector('.month-year').innerHTML = `${month} ${year}`;
-
-    const weekdays = document.querySelector('.weekdays');
-    weekdays.innerHTML = '';
-    ['Mon', 'Tue', 'Wed', 'Thur', 'Fri', 'Sat', 'Sun'].forEach(name => {
-        const weekday = document.createElement('li');
-        weekday.innerHTML = name;
-        weekdays.append(weekday);
-    })
-
-    let countdays = 0, daysOfMonth, flag = false;
-    // This loop counts the days from 1 up to the month in the given year
-    for (let yyyy = 1; yyyy <= year; yyyy++) {
-        for (let mm = 0; mm < 12; mm++) {
-            if (mm === 1) {  // February
-                if (yyyy % 4 === 0 && yyyy % 100 !== 0 || yyyy % 400 === 0) {  // Leap year
-                    daysOfMonth = 29;
-                } else { // other year
-                    daysOfMonth = 28;
-                }
-            } else if ([8, 3, 5, 10].includes(mm)) {
-                daysOfMonth = 30;
-            } else {
-                daysOfMonth = 31;
-            }
-            for (let dd = 1; dd <= daysOfMonth; dd++) {
-                countdays++;
-            }
-            if (yyyy === Number(year) && mm === months.indexOf(month)) {
-                flag = true;
-                break;
-            }
-        }
-        if (flag) break;
-    }
-
-    const days = document.querySelector('.days');
+    yearInput.value = year0;
+    calendarHeader.innerHTML = `${month} ${year0}`;
     days.innerHTML = '';
+
     // Fill blank days of month
     for (let i = 1; i <= (countdays - daysOfMonth) % 7; i++) {
         const day = document.createElement('li');
-        day.id = 'empty-day';
+        day.className = 'empty-day';
         days.append(day);
     }
     // Fill numbered days of month
@@ -131,6 +102,7 @@ function createCalendar() {
         if (year === now.getFullYear() && month === months[now.getMonth()] && i === now.getDate()) {
             day.id = 'today';
         }
+        day.setAttribute('title', countdays - daysOfMonth + i);
     }
 }
 
@@ -145,6 +117,33 @@ function handleSubmit(e) {
     year = Number(yearInput.value);
     yearInput.blur();
     createCalendar();
+}
+
+function countDateTime() {
+    let [countdays, daysOfMonth] = [0, 0];
+    for (let y = 1; y <= year; y++) {
+        let stopCount = false;
+        for (let m = 0; m < 12; m++) {
+            if (m === 1) {  // February
+                if (y % 4 === 0 && y % 100 !== 0 || y % 400 === 0) {  // Leap year
+                    daysOfMonth = 29;
+                } else { // other year
+                    daysOfMonth = 28;
+                }
+            } else if ([8, 3, 5, 10].includes(m)) {
+                daysOfMonth = 30;
+            } else {
+                daysOfMonth = 31;
+            }
+            countdays += daysOfMonth;
+            if (y === year && m === months.indexOf(month)) {
+                stopCount = true;
+                break;
+            }
+        }
+        if (stopCount) break;
+    }
+    return [countdays, daysOfMonth];
 }
 
 // /**What happened on this day - onthisday.com*/
